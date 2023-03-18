538702aec90f5fd0a0d397a8b20d86febd815d8c
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-03-18 21:40:40 +0300

    Add files via upload

---
+++ server/target/debug/deps/server-169538853c91afe6.d
@@ -0,0 +1,5 @@
+c:\Users\Карина\Desktop\client_server\server\target\debug\deps\server-169538853c91afe6.rmeta: src\main.rs
+
+c:\Users\Карина\Desktop\client_server\server\target\debug\deps\server-169538853c91afe6.d: src\main.rs
+
+src\main.rs:
---
+++ server/target/debug/deps/server.d
@@ -0,0 +1,5 @@
+C:\Users\Карина\Desktop\client_server\server\target\debug\deps\server.exe: src\main.rs
+
+C:\Users\Карина\Desktop\client_server\server\target\debug\deps\server.d: src\main.rs
+
+src\main.rs:
