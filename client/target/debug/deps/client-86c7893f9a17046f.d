1614c5d4227103b9049a5930c0da6c54dcc7e125
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-03-18 23:25:26 +0300

    client is done

---
+++ client/target/debug/deps/client-86c7893f9a17046f.d
@@ -0,0 +1,5 @@
+c:\Users\Карина\Desktop\client_server\client\target\debug\deps\client-86c7893f9a17046f.rmeta: src\main.rs
+
+c:\Users\Карина\Desktop\client_server\client\target\debug\deps\client-86c7893f9a17046f.d: src\main.rs
+
+src\main.rs:
