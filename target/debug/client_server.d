c82401f8db247776987c550e5edbc5c504603780
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-03-26 18:35:16 +0300

    Delete target directory

--- target/debug/client_server.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Карина\Desktop\client_server\target\debug\client_server.exe: C:\Users\Карина\Desktop\client_server\src\main.rs
--- target/debug/deps/client_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Карина\Desktop\client_server\target\debug\deps\client_server.exe: src\main.rs
-
-C:\Users\Карина\Desktop\client_server\target\debug\deps\client_server.d: src\main.rs
-
-src\main.rs:
