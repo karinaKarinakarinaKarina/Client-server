800cb5f7f29a167bdb0f3422925c5ac7aa30c308
Karina <103330695+karinaKarinakarinaKarina@users.noreply.github.com>
2023-03-26 18:35:29 +0300

    Delete client/target directory

--- client/target/debug/deps/client-dfb2b63364c8040b.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Карина\Desktop\client_server\client\target\debug\deps\client-dfb2b63364c8040b.rmeta: src\main.rs
-
-c:\Users\Карина\Desktop\client_server\client\target\debug\deps\client-dfb2b63364c8040b.d: src\main.rs
-
-src\main.rs:
